c2d5ed9829b5a0a0ed6882c2a7566adf2d186433
Karishtan <52981138+Karishtan@users.noreply.github.com>
2021-06-23 17:22:48 -0700

    Routes

--- app.js
+++ app.js
@@ -1,38 +1,25 @@
-const express = require("express");
-const ExpressError = require("./expressError");
-
-
-const item = require("./item")
-
+const express = require("express")
 const app = express();
+const itemsRoutes = require("./routes/items")
+const ExpressError = require("./expressError")
 
 app.use(express.json());
+app.use("/items", itemsRoutes);
 
+/** 404 handler */
 
-app.use('/items', item)
-
-
-
-
-// 404 handler
-app.use(function (req, res) {
-    return new ExpressError("Not Found", 404);
+app.use(function (req, res, next) {
+  return new ExpressError("Not Found", 404);
 });
 
-// generic error handler 
-app.use(function (err, req, res, next) {
-    //the default status is 500 Internal Server Error
-    let status = err.status || 500;
+/** general error handler */
 
-    //set the status and alert the user
-    return res.status(status).json({
-        error: {
-            message: err.message,
-            status: status
-        }
-    });
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+
+  return res.json({
+    error: err.message,
+  });
 });
 
-app.listen(3000, function () {
-    console.log("Server is listening on port 3000");
-});
+module.exports = app
--- item.js
+++ item.js
@@ -1,21 +1,51 @@
-const express = require('express');
-const router = new express.Router();
+/** Item in a shopping cart. */
+const items = require("./fakeDb")
 
-const ITEMS = [
-    { id: 1, name: "popsicle" },
-    { id: 2, name: "cheerios" },
-]
+class Item {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
 
-router.get('/', (req, res) => {
-    res.json({ items: ITEMS })
-})
+    // keep track of all items
+    items.push(this);
+  }
+
+  static findAll(){
+    return items
+  }
+  /** Update found item with matching name to data. */
+
+  static update(name, data) {
+    let foundItem = Item.find(name);
+    if (foundItem === undefined) {
+      throw {message: "Not Found", status: 404}
+    }
+    foundItem.name = data.name;
+    foundItem.price = data.price;
+
+    return foundItem;
+  }
+
+  /** Find & return item with matching name. */
+
+  static find(name) {
+    const foundItem = items.find(v => v.name === name);
+    if(foundItem === undefined){
+      throw { message: "Not Found", status: 404 }
+    }
+    return foundItem
+  }
+
+  /** Remove item with matching id. */
 
 
-router.get('/:id', (req, res) => {
-    const user = ITEMS.find(u => u.id === +req.params.id)
-    res.json({ user })
-})
+  static remove(name) {
+    let foundIdx = items.findIndex(v => v.name === name);
+    if (foundIdx === -1) {
+      throw {message: "Not Found", status: 404}
+    }
+    items.splice(foundIdx, 1);
+  }
+}
 
-
-
-module.exports = router;
+module.exports = Item;
